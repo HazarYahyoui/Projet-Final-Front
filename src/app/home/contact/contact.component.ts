093079e05b498ce23017304b6321b93d13da535b
HazarYahyaoui <yahyaoui.hazar@gmail.com>
2023-03-26 10:41:39 +0200

    Contact

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HomeService } from "../Service-Home/home.service";
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  contactForm?: FormGroup;
+  submitted = false;
+  constructor(private homeservice: HomeService, private router : Router) {}
+  ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      name : new FormControl ('', [Validators.required]),
+      email : new FormControl ('', [Validators.required]),
+      subject : new FormControl ('', [Validators.required]),
+      message : new FormControl ('', [Validators.required]),
+     });
+  }
+  contact(){
+    this.submitted = true;
+    if(this.contactForm?.invalid){
+      return;
+    }
+    console.log(this.contactForm?.value);
+    
+    this.homeservice.Contact(this.contactForm?.value).subscribe((response:any)=>{
+      console.log(response);
+      // this.router.navigateByUrl('/home/home-page');
+    },(error)=>{console.log(error);
+    })
 
+}
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -13,6 +13,9 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { CardEventComponent } from './events/card-event/card-event.component';
 import { ReservationEventComponent } from './events/reservation-event/reservation-event.component';
 import { DetailsEventComponent } from './events/details-event/details-event.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormModule } from '@coreui/angular';
 
 
 @NgModule({
@@ -31,7 +34,11 @@ import { DetailsEventComponent } from './events/details-event/details-event.comp
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormModule
   ]
 })
 export class HomeModule { }
